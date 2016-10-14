5b7ae102a09785c5290de47eecde9ed72df58442
MoAir <hassoun@outlook.com>
2016-10-14 09:45:24 -0400

    delete exception and use ShipstationError class instead

--- lib/exceptions/missing_attribute.rb
+++
@@ -1,9 +0,0 @@
-module Shipstation
-  module Exceptions
-    class MissingAttribute < StandardError
-      def initialize(msg='Some required attributes are missing')
-        super
-      end
-    end
-  end
-end
